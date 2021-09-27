b61f78c4aa3a36c0b34d58c1840c44c74d65191f
Jackson Magnabosco <jacksonmagnabosco@hotmail.com>
2021-09-27 14:20:59 -0300

    value coins

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,14 +1,36 @@
 require("../../public/javascripts/api");
 const axios = require("axios");
-const { wallet } = require("../../public/javascripts/carteira");
+const { wallet, walletBTC, walletETH, walletADA, walletLTC, walletBNB,walletALICE, walletBADGER, walletALPHA, walletCHZ, walletDOT } = require("../../public/javascripts/carteira");
 
 
 module.exports = function (app) {
 
   (async function () {
     var moeda = await wallet()
+    var btc = await walletBTC();
+    var moedaBTC = btc[0].free;
+    var eth = await walletETH();
+    var moedaETH = eth[0].free;
+    var ada = await walletADA();
+    var moedaADA = ada[0].free;
+    var ltc = await walletLTC();
+    var moedaLTC = ltc[0].free;
+    var bnb = await walletBNB();
+    var moedaBNB = bnb[0].free;
+    var alice = await walletALICE();
+    var moedaAlice = alice[0].free;
+    var badger = await walletBADGER();
+    var moedaBadger = badger[0].free;
+    var alpha = await walletALPHA();
+    var moedaAlpha = alpha[0].free;
+    var dot = await walletDOT();
+    var moedaDot = dot[0].free;
+    var chz = await walletCHZ();
+    var moedaChz = chz[0].free;
     app.get('/', function (req, res) {
-      res.render("index", { value: moeda });
+      res.render("index", { value: moeda, btcValue: moedaBTC, ethValue: moedaETH
+      , adaValue: moedaADA, ltcValue: moedaLTC, bnbValue: moedaBNB, aliceValue: moedaAlice, badgerValue: moedaBadger
+      , alphaValue: moedaAlpha, dotValue: moedaDot, chzValue: moedaChz  });
     });
   })()
 }
--- public/javascripts/carteira.js
+++ public/javascripts/carteira.js
@@ -4,20 +4,130 @@ const api = require("./api");
 const symbol = process.env.SYMBOL;
 
 async function wallet() {
-    try {
-      const account = await api.accountInfo();
-      const coins = account.balances.filter(b => symbol.indexOf(b.asset) !== -1);
-      let typeSymbol = coins.map((e,index)=> coins[index][Object.keys(e)[0]]);
-      let valueCoin = coins.map((e,index)=> coins[index][Object.keys(e)[1]]);
-      var subCoin = valueCoin[2].substr(0, 6);
-      var moeda = subCoin + " " + typeSymbol[2];
-      return await moeda
-    } catch (error) {
-        console.log("Error", error)
-        throw error
-    }
+  try {
+    const account = await api.accountInfo();
+    const coins = account.balances.filter(b => symbol.indexOf(b.asset) !== -1);
+    let typeSymbol = coins.map((e, index) => coins[index][Object.keys(e)[0]]);
+    let valueCoin = coins.map((e, index) => coins[index][Object.keys(e)[1]]);
+    var subCoin = valueCoin[2].substr(0, 6);
+    var moeda = subCoin + " " + typeSymbol[2];
+    return await moeda
+  } catch (error) {
+    console.log("Error", error)
+    throw error
+  }
+}
+
+async function walletBTC() {
+  try {
+    const carteira = await api.accountInfo();
+    const coins = carteira.balances.filter(b => b.asset === 'BTC')
+    return coins
+  } catch (error) {
+    console.log("Error", error)
+    throw error
+  }
+}
+
+async function walletETH() {
+  try {
+    const carteira = await api.accountInfo();
+    const coins = carteira.balances.filter(b => b.asset === 'ETH')
+    return coins
+  } catch (error) {
+    console.log("Error", error)
+    throw error
+  }
+}
+
+async function walletADA() {
+  try {
+    const carteira = await api.accountInfo();
+    const coins = carteira.balances.filter(b => b.asset === 'ADA')
+    return coins
+  } catch (error) {
+    console.log("Error", error)
+    throw error
+  }
+}
+
+async function walletLTC() {
+  try {
+    const carteira = await api.accountInfo();
+    const coins = carteira.balances.filter(b => b.asset === 'LTC')
+    return coins
+  } catch (error) {
+    console.log("Error", error)
+    throw error
+  }
+}
+
+async function walletBNB() {
+  try {
+    const carteira = await api.accountInfo();
+    const coins = carteira.balances.filter(b => b.asset === 'BNB')
+    return coins
+  } catch (error) {
+    console.log("Error", error)
+    throw error
+  }
+}
+
+async function walletALICE() {
+  try {
+    const carteira = await api.accountInfo();
+    const coins = carteira.balances.filter(b => b.asset === 'ALICE')
+    return coins
+  } catch (error) {
+    console.log("Error", error)
+    throw error
+  }
+}
+
+async function walletBADGER() {
+  try {
+    const carteira = await api.accountInfo();
+    const coins = carteira.balances.filter(b => b.asset === 'BADGER')
+    return coins
+  } catch (error) {
+    console.log("Error", error)
+    throw error
+  }
+}
+
+async function walletALPHA() {
+  try {
+    const carteira = await api.accountInfo();
+    const coins = carteira.balances.filter(b => b.asset === 'ALPHA')
+    return coins
+  } catch (error) {
+    console.log("Error", error)
+    throw error
+  }
+}
+
+async function walletDOT() {
+  try {
+    const carteira = await api.accountInfo();
+    const coins = carteira.balances.filter(b => b.asset === 'DOT')
+    return coins
+  } catch (error) {
+    console.log("Error", error)
+    throw error
+  }
+}
+
+async function walletCHZ() {
+  try {
+    const carteira = await api.accountInfo();
+    const coins = carteira.balances.filter(b => b.asset === 'CHZ')
+    return coins
+  } catch (error) {
+    console.log("Error", error)
+    throw error
+  }
 }
 
 module.exports = {
-  wallet
+  wallet, walletBTC, walletETH, walletADA, walletLTC, walletBNB, walletALICE, walletBADGER, walletALPHA, walletCHZ, walletDOT
 }
